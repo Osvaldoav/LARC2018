4fb5ff6b7beef29699b9d706743f1091d8b90419
AntonioRdzNav <antoniordznav@outlook.com>
2018-08-28 15:51:18 -0500

    Revert "Merge pull request #1 from Osvaldoav/toño"
    This reverts commit 9ffd1755e79addeac6ab479204022a0b402be87c, reversing
    changes made to cd57ab60ebac1e09037fafd9d34be84e0fd52a6a.

--- Arduino/Encoder.ino
+++
@@ -1,9 +0,0 @@
-//Interrup funcitons, add or substract to steps
-void encoderStep(){
-  //If we are moving forward
-  if (encoderState == 1)
-    steps++;
-  //else if we are moving backwards
-  else if (encoderState == 2)
-    steps--;
-}
