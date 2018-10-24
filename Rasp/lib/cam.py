d2f5353d3de44cece133bee19ac906508758ec7d
Osvaldoav <jorgeosvaldoav@gmail.com>
2018-10-24 05:48:41 -0500

    Testing communication

--- Rasp/lib/cam.py
+++ Rasp/lib/cam.py
@@ -5,7 +5,8 @@ class Cam:
 	def __init__(self, port):
 		self.port = port
 		self.cam = cv2.VideoCapture(port)
-		ret, self.image = self.cam.read()
+		self.cam.read()
+		# ret, self.image = self.cam.read()
 
 	# Takes a picture, saves it into the images folder and updates self.image
 	def shoot(self):
