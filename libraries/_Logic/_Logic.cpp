0fc82d82767c275102ba5cb24e8d637e92db89c2
AntonioRdzNav <antoniordznav@outlook.com>
2018-10-31 06:19:20 -0600

    pull

--- libraries/_Logic/_Logic.cpp
+++ libraries/_Logic/_Logic.cpp
@@ -121,7 +121,7 @@ void _Logic::stackToShip(){
 
     currentLevel = lastColor == 'R' ? 3 : lastColor == 'G' ? green_boxes%6 : blue_boxes%6;
     traductor->LcdPrint("lastColor", lastColor);
-    delay(1000);
+    // delay(1000);
     traductor->updateMechanismMovement(lastLevel, currentLevel, false);
  
     if(B){
@@ -226,6 +226,7 @@ void _Logic::shipToStack(){
         if(lastColor == 'R')
             traductor->setTrainLevel(false);
         traductor->moveAtrasHorizontal(); // izquierda hasta topar linea horizontal
+        traductor->waitForMechanism(); 
         traductor->updateMechanismMovement(currentLevel, lastLevel, false);  // eleva el stack para no chocar con los demas
         currentLevel = stacks[stack];
         angle = (stack/2 + 1)%2 != 0 ? 90 : -90;
