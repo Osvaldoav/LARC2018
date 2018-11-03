8611b46d004e8d4cfcb1c144c48a16713c51bbb5
Osvaldoav <jorgeosvaldoav@gmail.com>
2018-11-03 05:53:14 -0600

    blink position has been changed

--- Rasp/main.py
+++ Rasp/main.py
@@ -21,8 +21,8 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
 
-cam1 = Cam(2)
-cam2 = Cam(3)
+cam1 = Cam(3)
+cam2 = Cam(2)
 
 try:
 	brain = Algorithm()
